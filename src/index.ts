1e06a37006cc058d5671ecf8d54d78673b1be31d
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-06-06 20:44:36 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let dimensionDeseada: number = Number(prompt("Ingrese la dimensión deseada para el arreglo"));
+let valoresNumericos: number[] = new Array(dimensionDeseada);
+
+
+for (let indice = 0; indice < valoresNumericos.length; indice++) {
+  valoresNumericos[indice] = Math.floor(Math.random() * 100);
+  console.log(valoresNumericos[indice]);
+}
+
+let min = Math.min(...valoresNumericos);
+console.log(`el menor es ${min}`);
